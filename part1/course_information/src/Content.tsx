097161569976e175d030a88f8fa7b194f85162f3
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2024-01-25 14:56:48 +0200

    Add exercise 1.2

--- part1/course_information/src/Content.tsx
+++ part1/course_information/src/Content.tsx
@@ -1,14 +1,16 @@
+import Part from "./Part"
+
 interface ContentProps {
-    part: string
-    exercises: number
+    parts: string[]
+    exercises: number[]
 }
 
 const Content = (props: ContentProps) => {
     return (
         <>
-            <p>
-                {props.part} {props.exercises}
-            </p>
+            <Part part={props.parts[0]} exercises={props.exercises[0]} />
+            <Part part={props.parts[1]} exercises={props.exercises[1]} />
+            <Part part={props.parts[2]} exercises={props.exercises[2]} />
         </>
     )
 }
---
+++ part1/course_information/src/Part.tsx
@@ -0,0 +1,16 @@
+interface PartProps {
+    part: string
+    exercises: number
+}
+
+const Part = (props: PartProps) => {
+    return (
+        <>
+            <p>
+                {props.part} {props.exercises}
+            </p>
+        </>
+    )
+}
+
+export default Part
